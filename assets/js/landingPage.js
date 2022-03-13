58916abfd86528aa55e406732709b13b3e022b7b
Muhammad Ashraf <muashrafalii@gmail.com>
2022-03-13 23:54:09 +0200

    Landing Page Loader

--- assets/js/landingPage.js
+++ assets/js/landingPage.js
@@ -91,3 +91,8 @@ bottomEmailInput.addEventListener("focusout", function () {
     label.classList.remove("focused");
   }
 });
+
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+  loader.classList.add("hidden");
+});
