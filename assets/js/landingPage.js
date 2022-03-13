67ec2777eb6a37ec7a25da260a1ade84252831e4
Muhammad Ashraf <muashrafalii@gmail.com>
2022-03-13 15:42:27 +0200

    Landing Page JavaScript

--- assets/js/landingPage.js
+++ assets/js/landingPage.js
@@ -1,6 +1,11 @@
-const faqContainer = document.querySelectorAll(".faqContainer");
+// const & Variables
 
-function accordionToggler() {
+const faqContainer = document.querySelectorAll(".faqContainer");
+const topEmailInput = document.getElementById("topEmail");
+const bottomEmailInput = document.getElementById("bottomEmail");
+
+// Functions
+function toggleAccordion() {
   const thisSign = this.querySelector(".faqHeader p:nth-of-type(2)");
 
   if (this.classList.contains("open")) {
@@ -21,6 +26,68 @@ function accordionToggler() {
   thisSign.innerHTML = "&#215;";
 }
 
+function validateEmail(email) {
+  const emailReg =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  if (!email.nextElementSibling.classList.contains("error")) {
+    const errorP = document.createElement("p");
+    errorP.classList.add("error");
+    email.insertAdjacentElement("afterend", errorP);
+  }
+
+  if (email.value === "" || email.value === null) {
+    email.nextElementSibling.textContent = "Email is required";
+    return;
+  }
+
+  if (!emailReg.test(email)) {
+    email.nextElementSibling.textContent = "Your Email is Invalid";
+    return;
+  }
+}
+
+//Event Listeners
 faqContainer.forEach((faq) => {
-  faq.addEventListener("click", accordionToggler);
+  faq.addEventListener("click", toggleAccordion);
+});
+
+topEmailInput.addEventListener("input", () => {
+  bottomEmailInput.value = topEmailInput.value;
+});
+
+topEmailInput.addEventListener("focusin", function () {
+  const label = this.querySelector("#topEmail ~ label");
+  if (!label.classList.contains("focused")) {
+    label.classList.add("focused");
+  }
+});
+
+topEmailInput.addEventListener("focusout", function () {
+  const label = this.querySelector("#topEmail ~ label");
+
+  validateEmail(topEmailInput);
+  if (topEmailInput.value === "" || topEmailInput.value === "") {
+    label.classList.remove("focused");
+  }
+});
+
+bottomEmailInput.addEventListener("input", () => {
+  topEmailInput.value = topEmailInput.value;
+});
+
+bottomEmailInput.addEventListener("focusin", function () {
+  const label = this.querySelector("#bottomEmail ~ label");
+  if (!label.classList.contains("focused")) {
+    label.classList.add("focused");
+  }
+});
+
+bottomEmailInput.addEventListener("focusout", function () {
+  const label = this.querySelector("#bottomEmail ~ label");
+
+  validateEmail(bottomEmailInput);
+  if (bottomEmailInput.value === "" || bottomEmailInput.value === "") {
+    label.classList.remove("focused");
+  }
 });
